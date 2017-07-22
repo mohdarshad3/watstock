ea87b85a7cfc2d88e5599deebc596849bf6df96d
dreamsstudios <vikas@dreamsstudios.org>
2017-07-22 13:03:51 +0530

    gulp task

---
+++ js/app.module.js
@@ -0,0 +1,42 @@
+/*!
+
+ *
+
+ * Angle - Bootstrap Admin App + AngularJS Material
+
+ *
+
+ * Version: 3.5.4
+
+ * Author: @themicon_co
+
+ * Website: http://themicon.co
+
+ * License: https://wrapbootstrap.com/help/licenses
+
+ *
+
+ */
+
+
+
+// APP START
+
+// -----------------------------------
+
+
+
+(function() {
+
+    'use strict';
+
+
+
+    angular
+
+        .module('wtst', [ 
+            'app.routes'
+        ]);
+
+})();
+
